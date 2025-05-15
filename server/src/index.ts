7e5c14ff781f7c92b8e7cee4e8a8245bc39b55cc
Lakshya Bhardwaj <labhansh0501@gmail.com>
2025-05-15 15:54:41 +0530

    server modifiactions

--- server/src/index.ts
+++ server/src/index.ts
@@ -38,7 +38,7 @@ app.use("/teams", teamRoutes);
 
 // SERVER 
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+const port = Number(process.env.PORT) || 3000;
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server running on part ${port}`);
 });
