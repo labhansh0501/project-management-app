dbdd2cb5e8b65370444655a1f0cff6b30e3831d3
Lakshya Bhardwaj <labhansh0501@gmail.com>
2025-05-16 04:51:09 +0530

    client's TImeline  VIew changed

--- client/src/app/projects/TimelineView/index.tsx
+++ client/src/app/projects/TimelineView/index.tsx
@@ -11,7 +11,7 @@ type Props = {
 
 type TaskTypeItems = "task" | "milestone" | "project";
 
-const TimelineView = ({ id, setIsModalNewTaskOpen }: Props) => {
+const Timeline = ({ id, setIsModalNewTaskOpen }: Props) => {
   const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
   const {
     data: tasks,
@@ -31,7 +31,7 @@ const TimelineView = ({ id, setIsModalNewTaskOpen }: Props) => {
         end: new Date(task.dueDate as string),
         name: task.title,
         id: `Task-${task.id}`,
-        type: "task" as TaskType,
+        type: "task" as TaskTypeItems,
         progress: task.points ? (task.points / 10) * 100 : 0,
         isDisabled: false,
       })) || []
@@ -48,7 +48,7 @@ const TimelineView = ({ id, setIsModalNewTaskOpen }: Props) => {
   };
 
   if (isLoading) return <div>Loading...</div>;
-  if (error) return <div> An error occured while fetching tasks</div>;
+  if (error || !tasks) return <div>An error occurred while fetching tasks</div>;
 
   return (
     <div className="px-4 xl:px-6">
@@ -58,7 +58,7 @@ const TimelineView = ({ id, setIsModalNewTaskOpen }: Props) => {
         </h1>
         <div className="relative inline-block w-64">
           <select
-            className="focus:shadow-outline block w-full appearance-none rounded border border-gray-400 bg-white px-4 py-2 pr-8 leading-tight hover:border-gray-500 focus:outline-none dark:border-dark-secondary dark:bg-dark-secondary dark:text-white"
+            className="focus:shadow-outline block w-full appearance-none rounded border border-gray-400 bg-white px-4 py-2 pr-8 leading-tight shadow hover:border-gray-500 focus:outline-none dark:border-dark-secondary dark:bg-dark-secondary dark:text-white"
             value={displayOptions.viewMode}
             onChange={handleViewModeChange}
           >
@@ -68,26 +68,29 @@ const TimelineView = ({ id, setIsModalNewTaskOpen }: Props) => {
           </select>
         </div>
       </div>
+
       <div className="overflow-hidden rounded-md bg-white shadow dark:bg-dark-secondary dark:text-white">
         <div className="timeline">
-            <Gantt tasks = {ganttTasks} {...displayOptions}
+          <Gantt
+            tasks={ganttTasks}
+            {...displayOptions}
             columnWidth={displayOptions.viewMode === ViewMode.Month ? 150 : 100}
             listCellWidth="100px"
-            barBackground ={isDarkMode ? "#101214" : "#aeb8c2"}
-            barBackgroundSelectedColor={isDarkMode ? "#0000" : "#9ba1a6"}
-            />
+            barBackgroundColor={isDarkMode ? "#101214" : "#aeb8c2"}
+            barBackgroundSelectedColor={isDarkMode ? "#000" : "#9ba1a6"}
+          />
         </div>
         <div className="px-4 pb-5 pt-1">
-            <button 
-            className="flex items-center reounded bg-blue-primary px-3 py-2 text-white hover:bg-blue-600"
-            onClick={()=> setIsModalNewTaskOpen(true)}
-            >
-                Add New Task
-            </button>
+          <button
+            className="flex items-center rounded bg-blue-primary px-3 py-2 text-white hover:bg-blue-600"
+            onClick={() => setIsModalNewTaskOpen(true)}
+          >
+            Add New Task
+          </button>
         </div>
       </div>
     </div>
   );
 };
 
-export default TimelineView;
+export default Timeline;
