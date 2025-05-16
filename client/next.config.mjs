5b96bd00530566abce8c7adb5f5396212feba669
Lakshya Bhardwaj <labhansh0501@gmail.com>
2025-05-16 16:29:36 +0530

    add the aws s3 urls for imaes

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,6 +1,17 @@
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  imaes: {
+    remotePatterns : [
+      {
+        protocol : "https",
+        hostname : "products3-images.s3.eu-north-1.amazonaws.com",
+        port: "",
+        patname : "/**",
+      }
+    ]
+  }
 };
 
 export default nextConfig;
--- client/src/components/Sidebar/index.tsx
+++ client/src/components/Sidebar/index.tsx
@@ -39,7 +39,7 @@ const Sidebar = () => {
         </div>
         {/* TEAM  */}
         <div className="flex items-center gap-5 border-y-[1.5px] border-gray-200 px-8 py-4 dark:border-gray-700">
-          <Image src="/logo.png" alt="logo" width={40} height={40} />
+          <Image src="https://products3-images.s3.eu-north-1.amazonaws.com/logo.png" alt="logo" width={40} height={40} />
           <div>
             <h3 className="text-md font-bold tracking-wide dark:text-gray-200">
               EDROH TEAM
