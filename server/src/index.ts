b76109e79e9138206edbbef8de6ef2945b1533df
Lakshya Bhardwaj <labhansh0501@gmail.com>
2025-05-18 11:10:47 +0530

    add  create-user route

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,43 +1,44 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import helmet from 'helmet';
-import morgan from 'morgan';
-import bodyParser from 'body-parser';
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import bodyParser from "body-parser";  
 
-// ROUTE IMPORTS 
+// ROUTE IMPORTS
 
-import projectRoutes from './routes/projectsRoutes'
-import taskRoutes from './routes/taskRoutes'
-import searchRoutes from './routes/searchRoutes'
-import userRoutes from './routes/userRoutes'
+import projectRoutes from "./routes/projectsRoutes";
+import taskRoutes from "./routes/taskRoutes";
+import searchRoutes from "./routes/searchRoutes";
+import userRoutes from "./routes/userRoutes";
 import teamRoutes from "./routes/teamRoutes";
 
-// CONFIGURATION 
+// CONFIGURATION
 dotenv.config();
-const app =  express();
+const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({policy : "cross-origin"}));
-app.use(morgan('common'))
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-// ROUTES 
+// ROUTES
 
-app.get('/', (req , res) => {
-    res.send("This is home route")
+app.get("/", (req, res) => {
+  res.send("This is home route");
 });
 
-app.use('/projects', projectRoutes)
-app.use('/tasks', taskRoutes)
-app.use('/search', searchRoutes)
-app.use('/users' , userRoutes)
+app.use("/projects", projectRoutes);
+app.use("/tasks", taskRoutes);
+app.use("/search", searchRoutes);
+app.use("/users", userRoutes);
 app.use("/teams", teamRoutes);
 
-// SERVER 
 
+
+// SERVER
 const port = Number(process.env.PORT) || 3000;
 app.listen(port, "0.0.0.0", () => {
   console.log(`Server running on part ${port}`);
--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -1,8 +1,8 @@
 import { Router } from "express";
-import { getUsers } from "../controllers/userController"
+import { getUsers, postUser } from "../controllers/userController"
 
 const router = Router();
 
 router.get("/" , getUsers)
-
+router.get("/" , postUser)
 export default router;
